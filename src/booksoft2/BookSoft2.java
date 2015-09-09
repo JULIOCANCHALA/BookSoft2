afbf904c0ad7d5ba630a9c7262d4f152dd3c61f0
JULIOCANCHALA <juliofernando1c@hotmail.com>
2015-09-09 11:24:36 -0500

    Version 1.0 Version de  prueba de conexion a HitGub

--- src/booksoft2/BookSoft2.java
+++ src/booksoft2/BookSoft2.java
@@ -16,18 +16,34 @@ public class BookSoft2 {
         
         String user= "root";//local
         String pasword="1036661631";//local
-        String user2= "juliocanchala";//internet
-        String pasword2="1036661631";//internet
+        String user2= "sql289524";//internet
+        String pasword2="sK8!qL6%";//internet
         Scanner teclado=new Scanner(System.in);
 
             
         try {    
             System.out.println("Intentando conectar a la base de datos..");
-            Class.forName("com.mysql.jdbc.Driver");            
+            Class.forName("com.mysql.jdbc.Driver");       
             
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost/contactos",user,pasword);
-            //Connection con=DriverManager.getConnection("jdbc:mysql://db4free.net/contactosjc",user2,pasword2);//para internet      
+            
+            //Connection con=DriverManager.getConnection("jdbc:mysql://localhost/contactos",user,pasword);            
+            Connection con=DriverManager.getConnection("jdbc:mysql://sql2.freemysqlhosting.net/sql289524",user2,pasword2);//para internet      
             System.out.println("Conexion exitosa...");           
+            
+            
+            
+        //Cargar todos los contactos
+            Statement estado=con.createStatement(); 
+            System.out.println("Imprecion de tabla:");
+            ResultSet resultado=estado.executeQuery("SELECT * FROM `registro`");//traemos la tabla 
+            
+            while (resultado.next())
+            {//imprime todas las casillas de la tabla
+                System.out.println(resultado.getString("id")+ "\t" +resultado.getInt("codigo")
+                +"\t"+resultado.getString("nombre")+"\t"+resultado.getString("autor")+"\t"+resultado.getString("area")
+                        + "\t" +resultado.getInt("año")+ "\t" +resultado.getInt("cantidad"));
+            }
+            
             
         } catch (SQLException ex) {//error en la sintaxis de mysql
             System.out.println("Error de mysql");
