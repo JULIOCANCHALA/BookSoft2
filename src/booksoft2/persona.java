ca114c64414b994273ec0bf752ef5f41085b1afd
JULIOCANCHALA <juliofernando1c@hotmail.com>
2015-09-09 10:28:45 -0500

    Version 1.0 Version de  prueba de conexion a HitGub

---
+++ src/booksoft2/persona.java
@@ -0,0 +1,21 @@
+package booksoft2;
+
+/**
+ *
+ * @author Julio
+ */
+public class persona {
+    
+    private String cedula;
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    
+    
+    
+}
